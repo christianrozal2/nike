368f518a2e86131d6ff31014172130547221d0c2
christianrozal2 <christianrozal2@gmail.com>
2024-03-02 16:29:41 +0800

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+import PopularProducts from './PopularProducts';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Subscribe,
+    };
